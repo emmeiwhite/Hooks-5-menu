37a64ca23a9975b70dd8061f7be0f306ac5bb2d0
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-24 10:54:15 +0530

    Added CSS-Grid

--- src/components/menu-items/MenuItems.js
+++ src/components/menu-items/MenuItems.js
@@ -5,8 +5,17 @@ export default function MenuItems({ menulist }) {
   console.log(menulist);
   const menuList = menulist.map((menu) => (
     <div className="menu-item">
-      <p>{menu.title}</p>
-      <p>{menu.desc}</p>
+      <div className="menu-image-wrapper">
+        <img src={menu.img} alt="image" className="photo" />
+      </div>
+
+      <div className="menu-description">
+        <div className="menu-title-price">
+          <span className="menu-title">{menu.title}</span>
+          <span className="menu-price">$ {menu.price}</span>
+        </div>
+        <p className="menu-description-text">{menu.desc}</p>
+      </div>
     </div>
   ));
   return <div class="menulist-wrapper">{menuList}</div>;
